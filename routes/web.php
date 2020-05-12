1ac8966f1d13f49c0022d8fe0f1be7aaa2b06b78
hypeeakk <hyperakk@gmail.com>
2020-05-12 15:02:01 +0630

    add about, contact, blog, single_blog pages

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function(){
+	return view('contact');
+});
+Route::get('/blog', function(){
+	return view('blog');
+});
+Route::get('/single_blog', function(){
+	return view('single_blog');
+});
 
 Auth::routes();
 
