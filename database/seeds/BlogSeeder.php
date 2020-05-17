274c7a666a3df2bbf3f16e4ffcade12ae78b654b
BunnyShinny <shinewanna27199@gmail.com>
2020-05-17 19:17:34 +0630

    seeders for product

--- database/seeds/BlogSeeder.php
+++ database/seeds/BlogSeeder.php
@@ -15,7 +15,7 @@ class BlogSeeder extends Seeder
             [
                 "title"=> 'Architecture',
                 "description"=> 'You can learn various Bagan era Architecture (AD-1044 ~ AD-1256) from various pagodas that were built throughout the entire Bagan region.',
-                "image"=> '/assets/img/arch_1589549801.jpg',
+                "image"=> '/vegefoods/images/productss/thingstodo/arch.jpg',
                 "blog_category_id"=> '1',
             ],
 
--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -26,6 +26,27 @@ class ProductSeeder extends Seeder
                 "description"=> 'Myin Phyu Longyi',
                 "category_id"=> '1',
             ],
+            [
+                "name"=> 'Mote late pyar',
+                "price"=> '200',
+                "image"=> '/vegefoods/images/productss/Myamar-food/Snacks/mote late pyar/snack1.jpg',
+                "description"=> 'Myanmar Traditional Thanatkhar from local Thanatkhar Tree. It is good for skin.',
+                "category_id"=> '3',
+            ],
+            [
+                "name"=> 'La Phat Toke',
+                "price"=> '500',
+                "image"=> '/vegefoods/images/productss/Myamar-food/Snacks/la-phat-toke/the-infamous-tea-leaf.jpg',
+                "description"=> 'Traditional Myanmar Salad make with tea leaf and fried beans',
+                "category_id"=> '3',
+            ],
+            [
+                "name"=> 'Khaung Yay',
+                "price"=> '500',
+                "image"=> '/vegefoods/images/productss/Myamar-food/Drinks/khaung yay/khaung yay.jpg',
+                "description"=> 'Traditional Myanmar Salad make with tea leaf and fried beans',
+                "category_id"=> '4',
+            ],
 
         ]);
     }
