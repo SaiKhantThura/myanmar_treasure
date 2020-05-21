9a81719802fd612abc741581f821b0efb99f0f8e
Henry <saikhayshang79@gmail.com>
2020-05-21 23:18:12 +0630

    modified messaging-sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,7 +3,6 @@ Give the service worker access to Firebase Messaging.
 Note that you can only use Firebase Messaging here, other Firebase libraries are not available in the service worker.
 */
 importScripts('https://www.gstatic.com/firebasejs/7.14.2/firebase-app.js');
-// importScripts('https://www.gstatic.com/firebasejs/7.14.2/firebase-messaging.js');
 
 /*
 Initialize the Firebase app in the service worker by passing in the messagingSenderId.
@@ -18,25 +17,3 @@ firebase.initializeApp({
   messagingSenderId: "831127853700",
   appId: "1:831127853700:web:10b1d693217b727e0bc0d1"
 });
-
-/*
-Retrieve an instance of Firebase Messaging so that it can handle background messages.
-*/
-// const messaging = firebase.messaging();
-// messaging.setBackgroundMessageHandler(function(payload) {
-//   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-//   // Customize notification here
-//   const notificationTitle = 'Background Message Title';
-//   const notificationOptions = {
-//     body: 'Background Message body.',
-//     icon: '/firebase-logo.png'
-//   };
-
-//   return self.registration.showNotification(notificationTitle,
-//       notificationOptions);
-// });
-
-// messaging.onMessage((payload) => {
-//   console.log('Message received. ', payload);
-//   // ...
-// });
